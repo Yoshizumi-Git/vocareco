6eacca5d55eccce2a87075c7a90efcd88bda75b9
yoshizumi <yoshizumi1114@gmail.com>
2017-01-07 19:56:25 +0900

    test

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -8,6 +8,7 @@ class MusicController < ApplicationController
 
   def create
     like={"大好き" => 2,"好き" => 1,"知らない" => 0,"嫌い" => -1,"大嫌い" => -2}
+    print(params)
     rating = Rating.where(music_id:params[:music_id],user_id:current_user.id).first_or_initialize
     rating[:rate] = like[params[:rating]]
     rating.save
