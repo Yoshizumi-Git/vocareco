0139f065e09a641c1c4b5fb15466c16202397f8c
yoshizumi <yoshizumi1114@gmail.com>
2017-01-07 21:00:26 +0900

    rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    print(params)
     #Recommend.make_rating(current_user.id)
     @recommends = Recommend.find_by_sql(['select * from recommends where user_id = :id order by rating DESC limit 10', {id: current_user.id}])
   end
